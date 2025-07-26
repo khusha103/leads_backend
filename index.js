1ee94554a41e51780955dd50a6f8e04070023672
KhushaEkarigar <khushboo.sharma@ekarigar.com>
2025-07-26 15:15:58 +0530

    remote mysql

--- index.js
+++ index.js
@@ -32,17 +32,17 @@ app.use(cors());
 //   allowedHeaders: ['Content-Type', 'Authorization']
 // }));
 
-// app.use(cors({
-//   origin: 'https://ek-reps.com',
-//   credentials: true
-// }));
-
 app.use(cors({
   origin: 'https://staging.ekarigar.com',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+
+// app.use(cors({
+//   origin: 'https://staging.ekarigar.com',
+//   methods: ['GET', 'POST', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
 
 
 
