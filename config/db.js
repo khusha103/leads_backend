7ca7da1d200e51bc691511d56955dfb28358f545
KhushaEkarigar <khushboo.sharma@ekarigar.com>
2025-08-04 13:50:32 +0530

    new conncection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
  
 const dbConfig = {
-  host: 'localhost',           // or '127.0.0.1'
+  host: '127.0.0.1',           // or '127.0.0.1'
   port: 3306,                  // default MySQL port
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
