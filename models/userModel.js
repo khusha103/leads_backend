76d6cda6b064130590eaaa436749ed2aa898b803
Khusha <khusha.ss103@gmail.com>
2025-08-07 12:09:30 +0530

    routes test

--- models/userModel.js
+++ models/userModel.js
@@ -10,27 +10,7 @@ class UserModel {
     }
   }
 
-  static async findUserByEmail(email) {
-    try {
-      const [rows] = await db.query('SELECT * FROM ekarigar_users WHERE email = ?', [email]);
-      return rows[0];
-    } catch (err) {
-      throw new Error('Error finding user: ' + err.message);
-    }
-  }
-
-  static async createUser(userData) {
-    try {
-      const { name, email, password } = userData;
-      const [result] = await db.query(
-        'INSERT INTO ekarigar_users (name, email, password) VALUES (?, ?, ?)',
-        [name, email, password]
-      );
-      return { id: result.insertId, name, email };
-    } catch (err) {
-      throw new Error('Error creating user: ' + err.message);
-    }
-  }
+ 
 }
 
 module.exports = UserModel;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/users', authController.getUsers);
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+
 
 module.exports = router;
