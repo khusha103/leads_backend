fa4c0cce1cab9dba78dbb6b5f5143428ecc7d599
KhushaEkarigar <khushboo.sharma@ekarigar.com>
2025-08-07 11:33:09 +0530

    db.js0

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql2');
 require('dotenv').config();
- 
+
 const dbConfig = {
-  host: '127.0.0.1',           // or '127.0.0.1'
-  port: 3306,                  // default MySQL port
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 };
- 
+
 const pool = mysql.createPool(dbConfig).promise();
- 
+
 process.on('SIGINT', () => {
   pool.end(() => {
     console.log('MySQL connection pool closed.');
     process.exit();
   });
 });
- 
+
 module.exports = {
   query: async (...args) => {
     return pool.query(...args);
